5416b0e85ed3e6b7322ec636d134bc5b3d0e19f9
rania345 <raniatriek@gmail.com>
2024-02-05 18:32:30 +0100

    add file

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" the city class """
+"""city class"""
 from models.base_model import BaseModel
 
 class city(BaseModel):
-    """ describes a city, contains: state_id and name """
+    """describes a city.
+    Attributes:
+        state_id,
+        name.
+    """
 
     state_id = ""
     name = ""
